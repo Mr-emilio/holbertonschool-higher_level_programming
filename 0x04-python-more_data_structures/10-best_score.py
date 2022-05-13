9d00b084090180265db38356115271e1087545ce
Mr-emilio <pablopeag@hotmail.com>
2022-05-12 19:06:34 -0500

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        biggest_int = max(a_dictionary, key=lambda x: a_dictionary [x])
+        biggest_int = max(a_dictionary, key=lambda x: a_dictionary[x])
         return (biggest_int)
     return None
