979f858e9caf927a393ce17f1f94131a86287ff0
Mr-emilio <pablopeag@hotmail.com>
2022-05-12 19:01:37 -0500

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
-        return None
     if a_dictionary:
-        return max(zip(a_dictionary.values(), a_dictionaty.keys()))[1]
+        biggest_int = max(a_dictionary, key=lambda x: a_dictionary [x])
+        return (biggest_int)
+    return None
