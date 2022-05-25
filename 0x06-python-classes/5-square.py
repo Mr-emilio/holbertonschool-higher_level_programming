a264c0ad0eebd4fe3db6fe4be867598140bc33d8
Mr-emilio <pablopeag@hotmail.com>
2022-05-25 00:05:14 -0500

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""class square that defines a square"""
+
+
+class Square:
+    """Class square
+    attributes
+        size: type int, the side of a square
+    """
+    def __init__(self, size=0):
+        """instantiation of an square"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """return the size of square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """attributes size"""
+        if type(value) == int:
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """method that return the area of the square"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """prints the square with #"""
+        if self.__size != 0:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print("")
+        else:
+            print("")
