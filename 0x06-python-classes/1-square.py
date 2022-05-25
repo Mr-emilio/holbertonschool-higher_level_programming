28f49974adac2032215ad78440087ed99a78e5e7
Mr-emilio <pablopeag@hotmail.com>
2022-05-24 23:32:00 -0500

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class square that defines a square"""
+
+
+class Square:
+    """class square
+    attributes
+    size: type int, side of the square
+    """
+    def _init_(self, size):
+        """instantiation of an square"""
+        self.__size = size
